a79eab985b9c261bd0c1cae128c135697d1b1670
Trufanov Roman <roman.trufanov@waveaccess.ru>
2021-11-03 01:09:53 +0300

    add example of CLI project

---
+++ Shop/Shop.CLI/Commands/PlaceProductCommand.cs
@@ -0,0 +1,37 @@
+﻿using Shop.CLI.Models;
+using System;
+
+namespace Shop.CLI
+{
+    public class PlaceProductCommand : ICommand
+    {
+        private Store _store;
+
+        public PlaceProductCommand(Store store)
+        {
+            _store = store;
+        }
+
+        public string Name => "3) Разместить товар на ветрине";
+        public ConsoleKey Key => ConsoleKey.D3;
+
+        public void Run()
+        {
+            var productId = Guid.NewGuid();
+
+            if (_store.Products.TryGetValue(productId, out var product) == false)
+            {
+                Console.WriteLine($"Не найден продукт с идентификатором: {productId}");
+            }
+
+            var showcaseId = Guid.NewGuid();
+
+            if (_store.Showcases.TryGetValue(showcaseId, out var showcase) == false)
+            {
+                Console.WriteLine($"Не найдена витрина с идентификатором: {showcaseId}");
+            }
+
+            showcase.Products.Add(product);
+        }
+    }
+}
---
+++ Shop/Shop.CLI/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.CLI
+{
+    public class Menu
+    {
+        private readonly Dictionary<ConsoleKey, ICommand> _commands;
+
+        public Menu()
+        {
+            _commands = new Dictionary<ConsoleKey, ICommand>();
+        }
+
+        public void Print()
+        {
+            var stringBuilder = new StringBuilder("Нажмите\n");
+
+            foreach (var command in _commands)
+            {
+                stringBuilder.Append(command.Value.Name);
+            }
+            var message = stringBuilder.ToString();
+
+            Console.WriteLine(message);
+        }
+
+        public void AddCommand(ICommand command)
+        {
+            if (command is null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            _commands.Add(command.Key, command);
+        }
+
+        public void InvokeCommand(ConsoleKey input)
+        {
+            if (_commands.TryGetValue(input, out var command) == false)
+            {
+                Console.WriteLine($"Не найден обработчик для команды: {input}");
+                return;
+            }
+
+            command.Run();
+        }
+    }
+}
